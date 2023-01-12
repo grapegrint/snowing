669127943ea6650c480bcc877e280a262f3bd878
grapegrint <113498630+grapegrint@users.noreply.github.com>
2023-01-12 14:24:48 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+const body= document.querySelector("body");
+const MIN_DURATION= 10;
+
+
+
+function makeSnowFlake(){
+    const snowflake= document.createElement("div");
+    const delay= Math.random()*10;
+    const initialOpacity= Math.random();
+    const duration= Math.random()*20+ MIN_DURATION;
+    
+    snowflake.classList.add("snowflake");
+    snowflake.style.left= `${Math.random()*window.screen.width}px`; 
+    snowflake.style.animationDelay= `${delay}s`;
+    snowflake.style.opacity=initialOpacity;
+    snowflake.style.animation= `fall ${duration}s linear`;
+    // 이부분 recheck
+    body.appendChild(snowflake);
+
+    setTimeout(()=>{
+        body.removeChild(snowflake);
+    }, (duration+delay)*1000);
+    }
+    
+
+for (let index=0; index<1000; index++){
+    setTimeout(makeSnowFlake, 500*index);
+   
+}
+ 
+
